94fc2c8b3ad1988f022f774360fc86acca87d979
Sergio Junior <sergio.ltnj@gmail.com>
2022-03-25 13:14:19 -0300

    Mostrando primeiros gifs na tela

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -3,7 +3,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -12,18 +11,19 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String? _search;
   int _offset = 0;
 
-  Future<Map> _getGifs() async{
+  Future<Map> _getGifs() async {
     http.Response response;
     Uri _url;
 
-    if(_search == null){
-      _url = Uri.parse("https://api.giphy.com/v1/gifs/trending?api_key=bAWUImbu4ChAIb2dIodkJFbE0chInyLm&limit=20&rating=g");
-    }else{
-      _url = Uri.parse("https://api.giphy.com/v1/gifs/search?api_key=bAWUImbu4ChAIb2dIodkJFbE0chInyLm&q=$_search&limit=20&offset=$_offset&rating=g&lang=en");
+    if (_search == null) {
+      _url = Uri.parse(
+          "https://api.giphy.com/v1/gifs/trending?api_key=bAWUImbu4ChAIb2dIodkJFbE0chInyLm&limit=20&rating=g");
+    } else {
+      _url = Uri.parse(
+          "https://api.giphy.com/v1/gifs/search?api_key=bAWUImbu4ChAIb2dIodkJFbE0chInyLm&q=$_search&limit=20&offset=$_offset&rating=g&lang=en");
     }
     response = await http.get(_url);
     return json.decode(response.body);
@@ -34,28 +34,69 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: Image.network("https://developers.giphy.com/static/img/dev-logo-lg.gif"),
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.gif"),
         centerTitle: true,
       ),
       backgroundColor: Colors.black,
       body: Column(
-        children: const [
-          Padding(
+        children: [
+          const Padding(
             padding: EdgeInsets.only(top: 15),
             child: TextField(
               decoration: InputDecoration(
-                labelText: "Pesquise aqui",
-                labelStyle: TextStyle(
-                  color: Colors.white
-                ),
-                border: OutlineInputBorder()
-              ),
+                  labelText: "Pesquise aqui",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
               style: TextStyle(color: Colors.white, fontSize: 18.0),
               textAlign: TextAlign.center,
             ),
+          ),
+          Expanded(
+            child: FutureBuilder(
+                future: _getGifs(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                    case ConnectionState.none:
+                      return Container(
+                        width: 200,
+                        height: 200,
+                        alignment: Alignment.center,
+                        child: const CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.white),
+                          strokeWidth: 5.0,
+                        ),
+                      );
+                    default:
+                      if (snapshot.hasError) {
+                        return Container();
+                      } else {
+                        return _createGifTable(context, snapshot);
+                      }
+                  }
+                }),
           )
         ],
       ),
     );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: const EdgeInsets.all(10),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10, mainAxisSpacing: 10),
+        itemCount: snapshot.data["data"].length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        });
   }
 }
